19b32191ec55ba47d88fb1d62d17efdf4848c1b5
nsmith-lua <98421799+nsmith-lua@users.noreply.github.com>
2023-11-12 17:53:18 -0600

    Add files via upload

---
+++ module_5/test.py
@@ -0,0 +1,6 @@
+import pymongo
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.oai9kyi.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient("mongodb+srv://admin:<password>@cluster0.oai9kyi.mongodb.net/?retryWrites=true&w=majority")
+db = client.pytech
+print(db.list_collection_names())
