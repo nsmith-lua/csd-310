73f35673cc6790e5e5e6911d6b1e91c6ec133592
nsmith-lua <98421799+nsmith-lua@users.noreply.github.com>
2023-11-18 13:48:04 -0600

    Add files via upload
    fixed pytech_insert and pytech_queries files

--- module_5/pytech insert.py
+++ module_5/pytech insert.py
@@ -3,10 +3,9 @@ from pymongo import MongoClient
 client = MongoClient("mongodb+srv://admin:admin@cluster0.oai9kyi.mongodb.net/?retryWrites=true&w=majority")
 db = client["pytech"]
 collection = db["students"]
-#
-#post = {"_id":1007, "First Name": "Strider", "Last Name": "NA"}
-#post = {"_id":1008, "First Name": "Frodo", "Last Name": "Baggins"}
-post = {"_id": 1009, "First Name": "Gandolf", "Last Name": "Wizard"}
-#
-collection.insert_one(post)
-#pyth
+
+post1 = {"_id": 1007, "First Name": "Aragorn", "Last Name": "son of Arathorn"}
+post2 = {"_id": 1008, "First Name": "Frodo", "Last Name": "Baggins"}
+post3 = {"_id": 1009, "First Name": "Gandolf", "Last Name": "The Grey"}
+
+collection.insert_many([post1, post2, post3])
--- module_5/pytech queries.py
+++ module_5/pytech queries.py
@@ -1,15 +1,12 @@
 from pymongo import MongoClient
-client = MongoClient("mongodb+srv://admin:admin@cluster0.rsnru.mongodb.net/pytech?retryWrites=true&w=majority")
-db = client.pytech
-students = db.students
-student_list = students.find({})
-                     
-                     
-print("\n")
-for doc in student_list:
-    print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+client = MongoClient("mongodb+srv://admin:admin@cluster0.oai9kyi.mongodb.net/?retryWrites=true&w=majority")
+db = client["pytech"]
+students = db["students"]
 
-Gandolf = students.find_one({"student_id": "1009"})
 
-print("  Student ID: " + Gandolf["student_id"] + "\n  First Name: " + Gandolf["first_name"] + "\n  Last Name: " + Gandolf["last_name"] + "\n")
+results = students.find({})
+#students.find( {"_id": 1007})
 
+
+for result in results:
+    print(result)
